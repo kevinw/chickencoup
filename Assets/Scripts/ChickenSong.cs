1c6fdccc08548ce8472c406e5c9077b6adc2cb34
Kevin Watters <kevinwatters@gmail.com>
2018-01-27 16:46:40 -0500

    a music test scene

--- Assets/Scripts/ChickenSong.cs
+++ Assets/Scripts/ChickenSong.cs
@@ -1,16 +1,154 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+namespace ChickenCoup {
 
+
+[System.Serializable]
+public class Note
+{
+	public float time;
+	public ControllerButton button;
+}
+
+[System.Serializable]
+public class Song
+{
+	public List<Note> notes = new List<Note>();
+	public float seconds = 4f;
+}
+
+[ExecuteInEditMode]
 public class ChickenSong : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float StaffLength = 2.0f;
+	public float StaffLineSpace = 0.3f;
+
+	public LineRenderer[] lines;
+	public GameObject eggprefab;
+	public Transform eggParent;
+	public LineRenderer timeline;
+
+	void Start()
+	{
+		if (Application.isPlaying)
+		{
+			var song = GenerateSong(6);
+			PlaceEggs(song);
+			StartCoroutine(PlaySongPractice(song));
+		}
+
 	}
-	
-	// Update is called once per frame
+
+	public ChickenAnimator opponent;
+
+	public float DEBUG_TIME;
+
+	IEnumerator PlaySongPractice(Song song)
+	{
+		var currentTime = 0f;
+		var lastNote = 0;
+		var nextNote = song.notes[lastNote];
+		yield return null;
+		while (true)
+		{
+			currentTime += Time.deltaTime;
+			var currentTimeNormalized = currentTime / song.seconds;
+			DEBUG_TIME = currentTimeNormalized;
+			SetTime(currentTimeNormalized);
+			if (currentTimeNormalized > nextNote.time)
+			{
+				Debug.Log("note " + nextNote.button + " at time " + nextNote.time);
+
+				//
+				// PLAY A SQUAK HERE
+				//
+
+				opponent.Squawk();
+				lastNote++;
+				if (lastNote < song.notes.Count)
+				{
+					nextNote = song.notes[lastNote];
+				}
+			}
+
+			yield return null;
+		}
+	}
+
+	public void SetTime(float t)
+	{
+		var pos = timeline.transform.localPosition;
+		pos.x = t * StaffLength;
+		timeline.transform.localPosition = pos;
+	}
+
+	static Color ColorForButton(ControllerButton button)
+	{
+		switch (button)
+		{
+			case ControllerButton.A: return Color.green;
+			case ControllerButton.B: return Color.red;
+			case ControllerButton.X: return Color.blue;
+			case ControllerButton.Y: return Color.yellow;
+			default: throw new System.NotImplementedException();
+		}
+
+	}
+
+	public static ControllerButton[] ButtonIndexes = new [] {
+		ControllerButton.A,
+		ControllerButton.X,
+		ControllerButton.B,
+		ControllerButton.Y
+	};
+
+	static int IndexForButton(ControllerButton button)
+	{
+		return System.Array.IndexOf(ButtonIndexes, button);
+	}
+
+	void PlaceEggs(Song song)
+	{
+		var notes = song.notes;
+		foreach (var note in notes)
+		{
+			var index = IndexForButton(note.button);
+			var pos = new Vector3(note.time * StaffLength, index * StaffLineSpace, -0.1f);
+			var eggObj = Instantiate(eggprefab, pos, Quaternion.identity);
+			eggObj.transform.SetParent(eggParent, false);
+			var eggSprite = eggObj.GetComponent<SpriteRenderer>();
+			eggSprite.color = ColorForButton(note.button);
+
+		}
+	}
+
 	void Update () {
-		
+		if (!Application.isPlaying)
+		{
+			for (var i = 0; i < lines.Length; ++i)
+			{
+				var line = lines[i];
+				line.SetPosition(1, new Vector3(StaffLength, 0, 0));
+				var pos = line.transform.localPosition;
+				pos.y = StaffLineSpace * (float)i;
+				line.transform.localPosition = pos;
+			}
+		}
 	}
+
+	Song GenerateSong(int numNotes)
+	{
+		var song = new Song();
+		for (var i = 0; i < numNotes; ++i)
+		{
+			var button = (ControllerButton)Random.Range(0, 4);
+			song.notes.Add(new Note{time = (float)i/(float)numNotes, button=button});
+		}
+
+		return song;
+
+	}
+}
+
 }
