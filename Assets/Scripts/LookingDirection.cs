b2d2fdf90fbb9f218140241b2d661e93cd1d2e1b
Kevin Watters <kevinwatters@gmail.com>
2018-01-26 23:28:38 -0500

    chicken line

---
+++ Assets/Scripts/LookingDirection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookingDirection : MonoBehaviour {
+	Vector3 lastPosition;
+	public Vector3 direction;
+
+	void Update () {
+		direction = Vector3.ProjectOnPlane(transform.position - lastPosition.normalized, Vector3.one);
+	}
+}
