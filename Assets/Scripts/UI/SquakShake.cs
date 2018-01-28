ceae2bf4e7f39e9f3ea9c21582de17aa43a34eea
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2018-01-28 11:28:10 -0500

    update noise prefab

--- Assets/Scripts/UI/SquakShake.cs
+++ Assets/Scripts/UI/SquakShake.cs
@@ -11,19 +11,23 @@ namespace ChickenCoup
 		[Range(0,100)]
 		public int amplitude;
 		// Use this for initialization
+		public bool shake;
 		void Start () {
 			Events.Noise.IncreaseNoise += OnNoiseIncreased;
 		}
 		
 		void OnNoiseIncreased(int val)
 		{
-			transform.Rotate(Vector3.forward, Mathf.PingPong(-5,5));
+
 		}
 
 		void Update()
 		{
-			float val = amplitude * Mathf.Sin(Time.time * shakeFactor);
-			transform.Rotate(Vector3.forward, val);
+			if(shake)
+			{
+				float val = amplitude * Mathf.Sin(Time.time * shakeFactor);
+				transform.Rotate(Vector3.forward, val);
+			}
 		}
 	}
 }
