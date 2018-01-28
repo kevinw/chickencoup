78ce14310fe18c7fc88145ec86ed7776f7da3321
Kevin Watters <kevinwatters@gmail.com>
2018-01-28 16:27:42 -0500

    end game

--- Assets/Nirvana.cs
+++ Assets/Nirvana.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 namespace ChickenCoup {
 
 public class Nirvana : MonoBehaviour {
+	static float AfterTriggerEndGameSeconds = 5.0f;
 	public static void ReachNirvana(GameObject obj)
 	{
 		var enlightenment = obj.GetComponent<RaiseToSky>();
 		if (!enlightenment)
 		{
 			obj.AddComponent<RaiseToSky>();
-			//if (obj.tag != "Player")
 			{
 				var b = obj.GetComponent<Rigidbody>();
 				b.useGravity = false;
@@ -19,12 +19,27 @@ public class Nirvana : MonoBehaviour {
 		}
 	}
 
+	static bool endGameTriggered;
+
+	void EndGame()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("results");
+
+	}
+
 	void OnTriggerEnter(Collider collider)
 	{
 		var recruitable = collider.GetComponent<Recruitable>();
 		var coreChicken = collider.GetComponent<CoreChicken>();
 		if (recruitable || coreChicken)
+		{
 			ReachNirvana(collider.gameObject);
+			if (coreChicken && !endGameTriggered)
+			{
+				endGameTriggered = true;
+				Invoke("EndGame", AfterTriggerEndGameSeconds);
+			}
+		}
 	}
 }
 
