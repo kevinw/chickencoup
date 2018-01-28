a390c0b0e232b23195c9e5977da8311f361ce4c9
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2018-01-27 23:51:41 -0500

    squak shake

---
+++ Assets/Scripts/UI/SquakShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChickenCoup
+{
+	public class SquakShake : MonoBehaviour {
+
+		[Range(0,100)]
+		public int shakeFactor;
+		[Range(0,100)]
+		public int amplitude;
+		// Use this for initialization
+		void Start () {
+			Events.Noise.IncreaseNoise += OnNoiseIncreased;
+		}
+		
+		void OnNoiseIncreased(int val)
+		{
+			transform.Rotate(Vector3.forward, Mathf.PingPong(-5,5));
+		}
+
+		void Update()
+		{
+			float val = amplitude * Mathf.Sin(Time.time * shakeFactor);
+			transform.Rotate(Vector3.forward, val);
+		}
+	}
+}
