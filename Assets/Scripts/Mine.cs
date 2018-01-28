c48ae45b8a98117eaabf61cbbe15c7fad3122aa1
Kevin Watters <kevinwatters@gmail.com>
2018-01-27 23:42:28 -0500

    mine explosions and chicken prefab

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
 
 namespace ChickenCoup {
 
 public class Mine : MonoBehaviour {
+	[FMODUnity.EventRef] public string ExplodeSound;
 	public float ExplosiveForce = 100f;
 	public float ExplosionRadius = 2f;
 	public float ExplosionUpwards = 3f;
     void OnTriggerEnter(Collider other) {
 		var rb = other.GetComponent<Rigidbody>();
+
+		if (!string.IsNullOrEmpty(ExplodeSound))
+			FMODUnity.RuntimeManager.PlayOneShot(ExplodeSound, transform.position);
+
 		if (rb)
 			rb.AddExplosionForce(ExplosiveForce, transform.position, ExplosionRadius, ExplosionUpwards);
 
