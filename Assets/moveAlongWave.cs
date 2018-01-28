142eb30fa61d2384ce0af12f9d40c79ee87bce0e
obriencole11 <obriencole11@gmail.com>
2018-01-28 14:23:03 -0500

    Added results screen

---
+++ Assets/moveAlongWave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveAlongWave : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Mathf.Sin((Time.time + startPosition.x) * 5.0f) * 0.25f;
+		transform.position = new Vector3(startPosition.x, startPosition.y + offset, startPosition.z);
+	}
+}
