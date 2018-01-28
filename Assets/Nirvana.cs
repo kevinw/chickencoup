8eb23ad289a9de2b8387ce55531fcd843e645518
Kevin Watters <kevinwatters@gmail.com>
2018-01-28 15:21:54 -0500

    chicken nirvana

---
+++ Assets/Nirvana.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChickenCoup {
+
+public class Nirvana : MonoBehaviour {
+	public static void ReachNirvana(GameObject obj)
+	{
+		var enlightenment = obj.GetComponent<RaiseToSky>();
+		if (!enlightenment)
+		{
+			obj.AddComponent<RaiseToSky>();
+			if (obj.tag != "Player")
+			{
+				var b = obj.GetComponent<Rigidbody>();
+				b.useGravity = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		var recruitable = collider.GetComponent<Recruitable>();
+		var coreChicken = collider.GetComponent<CoreChicken>();
+		if (recruitable || coreChicken)
+			ReachNirvana(collider.gameObject);
+	}
+}
+
+}
---
+++ Assets/RaiseToSky.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChickenCoup {
+public class RaiseToSky : MonoBehaviour {
+
+	public float RandomSpeedMin = 4.0f;
+	public float RandomSpeedMax = 9.0f;
+
+	float Speed;
+
+	void Start () {
+		var v = GetComponent<Verlet3D>();
+		if (v) v.enabled = false;
+
+		var follow = GetComponent<VerletFollow>();
+		if (follow) follow.enabled = false;
+
+		Speed = Random.Range(RandomSpeedMin, RandomSpeedMax);
+	}
+
+	void FixedUpdate()
+	{
+		var p = transform.position;
+		p.y += Speed * Time.deltaTime;
+		transform.position = p;
+	}
+}
+
+}
