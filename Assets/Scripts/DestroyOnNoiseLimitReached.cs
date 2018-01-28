73bf531951c0f3e9b23a76a42b69c81a6c68cf50
Kevin Watters <kevinwatters@gmail.com>
2018-01-28 11:59:46 -0500

    invisible wall in barn at first

---
+++ Assets/Scripts/DestroyOnNoiseLimitReached.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChickenCoup {
+public class DestroyOnNoiseLimitReached : MonoBehaviour {
+	void OnEnable () {
+		Events.Noise.NoiseLimitReached += OnNoiseLimitReached;
+	}
+
+	void OnDisable() {
+		Events.Noise.NoiseLimitReached -= OnNoiseLimitReached;
+	}
+
+	void OnNoiseLimitReached()
+	{
+		Destroy(gameObject);
+	}
+}
+}
