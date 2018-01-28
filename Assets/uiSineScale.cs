a0c57ce7fd9b10d6af0ee2d1330c1f1eae1b79f4
obriencole11 <obriencole11@gmail.com>
2018-01-28 02:31:32 -0500

    Added logo and text to main menu

---
+++ Assets/uiSineScale.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiSineScale : MonoBehaviour {
+
+    private RectTransform rect;
+    float startScale;
+    public float frequency = 2.0f;
+    public float amplitude = 0.1f;
+    public float offset = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        rect = gameObject.GetComponent<RectTransform>();
+        startScale = rect.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float value = (Mathf.Sin(Time.time * frequency + offset) * amplitude) + startScale;
+        rect.localScale = new Vector3(value, value, value);
+	}
+}
