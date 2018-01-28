625fe942bbe5b55953bde163daa52e77fb1628fc
Kevin Watters <kevinwatters@gmail.com>
2018-01-27 23:25:34 -0500

    add to the dolly track at the end

---
+++ Assets/Scripts/ExtendDolly.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChickenCoup
+{
+public class ExtendDolly : MonoBehaviour {
+
+	public Vector3 ExtraPoint;
+
+	void Start () {
+		Events.Noise.NoiseLimitReached += OnNoiseLimit;
+	}
+
+	void OnNoiseLimit()
+	{
+		var path = GetComponent<Cinemachine.CinemachineSmoothPath>();
+		System.Array.Resize(ref path.m_Waypoints, path.m_Waypoints.Length + 1);
+		path.m_Waypoints[path.m_Waypoints.Length - 1].position = ExtraPoint;
+		path.m_Waypoints[path.m_Waypoints.Length - 1].roll = 0;
+	}
+	
+}
+
+}
